e4e61a96b506977edd09fbb32a517f0595048a6d
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-28 20:45:38 -0700

    add new

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,14 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {createUserWithEmailAndPassword } from "firebase/auth";
+import auth from '../../../utils/firebase.config';
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: '',
+  email: '',
+  isLoading: true,
+  isError: false,
+  error: '',
 };
+
+export const createUser = createAsyncThunk('userSlice/createUser', 
+async({email, password})=>{
+  const data = await createUserWithEmailAndPassword(auth, email, password);
+console.log(data)
+  return;
+})
 
 const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {},
+  extraReducers: () =>
 });
 
 export default userSlice.reducer;
