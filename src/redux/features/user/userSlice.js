bce45b67414ce2fe8e870a2d7f16bdde3b0ade88
tufaelkhan <tufaelkhan247@gmail.com>
2023-09-28 21:02:08 -0700

    add payload

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -22,13 +22,26 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(createUser.pending, (state, {payload}) => {
+    builder.addCase(createUser.pending, (state) => {
       state.isLoading = true;
       state.isError = false;
       state.email = '';
       state.name = '';
       state.error = '';
-
+    })
+    .addCase(createUser.fulfilled, (state, {payload}) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.email = payload.email;
+      state.name = payload.name;
+      state.error = '';
+    })
+    .addCase(createUser.rejected, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.email = '';
+      state.name = '';
+      state.error = '';
     });
   }
 });
